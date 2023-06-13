3729584906aaecb29f69a014e9191305ab7ec3a0
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-14 00:13:08 +0200

    install frame motion

--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import styles from '@/styles/home-page/hero.module.css';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className="container">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum saepe
-          dignissimos amet explicabo assumenda, deleniti libero id quibusdam
-          odio commodi.
-        </p>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <h1 className={styles.title}>
+              Front-End Insights: Harnessing the Power of Design and Technology
+              for Web Excellence
+            </h1>
+            <p className={styles.description}>
+              We are a team of passionate designers and developers who are
+              dedicated to delivering the best possible solutions for our
+              clients. We are a team of passionate designers and developers who
+              are dedicated to delivering the best possible solutions for our
+              clients.
+            </p>
+          </div>
+          <div className={styles.right}>
+            <Image
+              src="/images/home-page/hero-image.png"
+              alt="hero image"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
