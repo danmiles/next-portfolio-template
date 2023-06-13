9bafc6d47379e9404a6ae5d2268d0c8376d2da97
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-14 01:00:06 +0200

    add title animation

--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -4,29 +4,73 @@ import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 const Hero = () => {
+  const line1 = 'Front-End Insights: Harnessing the Power of Design ';
+  const line2 = 'and Technology for Web Excellence';
+
+  const sentence = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        staggerChildren: 0.08,
+      },
+    },
+  };
+
+  const letter = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
     <section className={styles.hero}>
       <div className="container">
         <div className={styles.content}>
           <div className={styles.left}>
-            <h1 className={styles.title}>
-              Front-End Insights: Harnessing the Power of Design and Technology
-              for Web Excellence
-            </h1>
+            <motion.h1
+              className={styles.title}
+              variants={sentence}
+              initial="hidden"
+              animate="visible"
+            >
+              {line1.split('').map((char, index) => {
+                return (
+                  <motion.span key={char + '-' + index} variants={letter}>
+                    {char}
+                  </motion.span>
+                );
+              })}
+              <br />
+              {line2.split('').map((char, index) => {
+                return (
+                  <motion.span key={char + '-' + index} variants={letter}>
+                    {char}
+                  </motion.span>
+                );
+              })}
+            </motion.h1>
             <p className={styles.description}>
               We are a team of passionate designers and developers who are
               dedicated to delivering the best possible solutions for our
-              clients. We are a team of passionate designers and developers who
-              are dedicated to delivering the best possible solutions for our
               clients.
+            </p>
+            <p className={styles.description}>
+              Next 13 is a revolutionary software solution that offers a wide
+              range of benefits to businesses and individuals alike. With Next
+              13, you can streamline your operations, enhance productivity, and
+              stay ahead of the competition. 
             </p>
           </div>
           <div className={styles.right}>
             <Image
               src="/images/home-page/hero-image.png"
               alt="hero image"
-              width={500}
-              height={500}
+              width={600}
+              height={600}
             />
           </div>
         </div>
