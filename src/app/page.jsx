a5dc5e9cd5416ebfe1aba0d423f2f5e50ae2ff0e
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-13 23:10:18 +0200

    start hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
-import styles from '@/styles/home.module.css';
+'use client';
+import Hero from '@/sections/home/Hero';
 
 export default function Home() {
   return (
-    <section className={styles.home}>
-      <div className={styles.container}></div>
-    </section>
+    <>
+      <Hero />
+    </>
   );
 }
---
+++ src/sections/home/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '@/styles/home-page/hero.module.css';
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <div className="container">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum saepe
+          dignissimos amet explicabo assumenda, deleniti libero id quibusdam
+          odio commodi.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
