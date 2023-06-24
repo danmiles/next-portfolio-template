a64ec3f6197d1b6a9ad601847d74d1b894e611e2
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-24 14:10:39 +0200

    Portfolio cactegory small Data changing

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import styles from "@/styles/pages/portfolioCategory.module.css";
-import ButtonLink from "@/components/ButtonLink.jsx";
-import Image from "next/image";
-import { items } from "./data.js";
-import { notFound } from "next/navigation";
+import React from 'react';
+import styles from '@/styles/pages/portfolioCategory.module.css';
+import ButtonLink from '@/components/ButtonLink.jsx';
+import Image from 'next/image';
+import { portfolioCategory } from '@/data/constData';
+import { notFound } from 'next/navigation';
 
 const getData = (cat) => {
-  const data = items[cat];
+  const data = portfolioCategory[cat];
 
   if (data) {
     return data;
@@ -18,27 +18,29 @@ const getData = (cat) => {
 const Category = ({ params }) => {
   const data = getData(params.category);
   return (
-    <div className='container'>
-      <h1 className={styles.catTitle}>{params.category}</h1>
+    <section className={styles.portfolioCategory}>
+      <div className="container">
+        <h1 className={styles.catTitle}>{params.category}</h1>
 
-      {data.map((item) => (
-        <div className={styles.item} key={item.id}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.desc}</p>
-            <ButtonLink text="See More" url="#" />
+        {data.map((item) => (
+          <div className={styles.item} key={item.id}>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.desc}>{item.desc}</p>
+              <ButtonLink text="See More" url="#" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                fill={true}
+                src={item.image}
+                alt=""
+              />
+            </div>
           </div>
-          <div className={styles.imgContainer}>
-            <Image
-              className={styles.img}
-              fill={true}
-              src={item.image}
-              alt=""
-            />
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
--- src/data/constData.jsx
+++ src/data/constData.jsx
@@ -1,3 +1,5 @@
+import Category from "@/app/portfolio/[category]/page";
+
 // Navbar links here
 export const navbarLinks = [
   {
@@ -51,3 +53,77 @@ export const aboutData = [
     description: 'Your bisiness is our priority and we provides high-quality software development services to its clients worldwide.',
   },
 ];
+
+// Portfolio Category 
+export const portfolioCategory = {
+  applications: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+    }
+  ],
+  illustrations: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+    }
+  ],
+  websites: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+    }
+  ],
+};
+
