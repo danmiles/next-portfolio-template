ed9f20d5ea300e7f3252fa18ea3c21cc0fe74fb5
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-26 22:34:17 +0200

    Mongoose shhema for Users and Posts

---
+++ src/models/Post.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
