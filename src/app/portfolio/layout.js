d046153758e9222a639575aa8d49d7287dac4b80
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-27 20:46:42 +0200

    Add layout SEO for portfolio page

---
+++ src/app/portfolio/layout.js
@@ -0,0 +1,12 @@
+// Becouse category page using 'use client' and function Metadata dosen't work in client rendering. 
+// I decided create this layout.js only for SEO.
+
+export const metadata = {
+  title: 'JsCase | Portfolio',
+  description:
+    'Our portfolio page of JsCase. We create digital products for your business.',
+};
+
+export default function RootLayout({ children }) {
+  return <>{children};</>;
+}
