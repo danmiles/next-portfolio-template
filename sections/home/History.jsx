51205097c7e1a9c539c98bc3d1c9bf46cff1d89f
Denis <41701228+danmiles@users.noreply.github.com>
2023-07-07 21:59:42 +0200

    NCU -U update

--- sections/home/History.jsx
+++ sections/home/History.jsx
@@ -6,23 +6,20 @@ import { motion } from 'framer-motion';
 import { TypingText } from '@components/CustomTexts';
 import { overturnFadeIn, fadeIn, staggerContainer } from '@utils/motion';
 
-
 const History = () => {
   return (
-    <section className={styles.history}>
+    <section className={`${styles.history} bg-linear-gradient`}>
       <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.35 }}
-        className={styles.container}
+        className="container"
       >
         <div className={styles.content}>
-
-
           <motion.div
             variants={fadeIn('right', 'tween', 0.2, 1)}
-            className={styles.left}        
+            className={styles.left}
           >
             <TypingText stylesClass="section-title" text="| history Us" />
             <h3 className={styles.subtitle}>
@@ -38,7 +35,7 @@ const History = () => {
           </motion.div>
 
           <motion.div
-            variants={overturnFadeIn('right')}          
+            variants={overturnFadeIn('right')}
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             className={styles.right}
