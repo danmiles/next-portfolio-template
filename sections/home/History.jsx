d43316ee004097a196723d8327d9faa65c47cfb9
Denis <41701228+danmiles@users.noreply.github.com>
2023-07-08 14:17:46 +0200

    history section need fix bug with animation

--- sections/home/History.jsx
+++ sections/home/History.jsx
@@ -13,38 +13,69 @@ const History = () => {
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
-        viewport={{ once: false, amount: 0.35 }}
+        viewport={{ once: false, amount: 0.25 }}
         className="container"
       >
         <div className={styles.content}>
           <motion.div
-            variants={fadeIn('left', 'tween', 0.2, 1)}
+            variants={fadeIn('right', 'tween', 0.2, 1)}
             className={styles.left}
           >
-            <TypingText stylesClass="section-title" text="| history Us" />
-            <h3 className={styles.subtitle}>
-              JsCase is a software development company that provides
-              high-quality software development services to its clients
-              worldwide.
-            </h3>
+            <TypingText stylesClass="section-title" text="| Our History" />
 
-            <p className={styles.text}>
-              So let's go on this wonderful digital adventure together. And we
-              will bring your project to new heights and successes.
-            </p>
+            <div className={styles.description}>
+              <h3 className={styles.subtitle}>
+                We have gone from freelance developers to a full-fledged team of
+                professionals. And we are ready to help you with your project.
+              </h3>
+
+              <p className={styles.text}>
+                So let's go on this wonderful digital adventure together. And we
+                will bring your project to new heights and successes.
+              </p>
+            </div>
+
+            <div className={styles.info}>
+              <div className={styles.devider}></div>
+              <div className={styles.infoContent}>
+                <p className={styles.infoText}>
+                  In the early days of web development, a small team of
+                  enthusiastic programmers founded JS Case in 1998.
+                  Armed with their passion for the emerging field and a vision
+                  for the future, they set out to create innovative and
+                  user-friendly websites for businesses. As the internet gained
+                  popularity and businesses realized the importance of having an
+                  online presence, JS Case quickly grew in reputation
+                  and size. Their talented team continuously adapted to the
+                  ever-evolving technologies, mastering HTML, CSS, JavaScript,
+                  and other programming languages.
+                  
+                </p>
+                <p className={styles.infoText}>
+                  Over the years, JS Case solidified its position as a
+                  leading web development company. Their success was attributed
+                  to a customer-centric approach, with a focus on understanding
+                  clients' unique needs and providing tailored solutions. The
+                  company expanded its services beyond web development, offering
+                  comprehensive digital strategies, e-commerce solutions, and
+                  mobile app development. With a team of skilled designers,
+                  developers, and digital marketers, JS Case delivered
+                  projects that not only impressed clients but also achieved
+                  outstanding results. 
+                </p>
+              </div>
+            </div>
           </motion.div>
 
           <motion.div
-            variants={overturnFadeIn('right')}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
+            variants={fadeIn('left', 'tween', 0.2, 1)}    
             className={styles.right}
           >
             <Image
               src="/images/home-page/history/our-history.png"
               alt="our history image"
-              width={700}
-              height={500}
+              width={600}
+              height={400}
               className={styles.historyImage}
             />
           </motion.div>
